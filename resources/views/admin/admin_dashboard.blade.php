ad0b48c6388bb35115ad6d5c68b4c4eff837fba6
papadiouf13 <papadiouf93@gmail.com>
2023-09-27 16:33:28 +0200

    Modifier Admin

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -16,6 +16,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+    
     <!-- End fonts -->
 
     <!-- core:css -->
@@ -36,6 +37,7 @@
     <!-- End layout styles -->
 
     <link rel="shortcut icon" href="{{ asset('backend/assets/images/favicon.png') }}" />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
 </head>
 
 <body>
@@ -77,7 +79,33 @@
 
     <!-- Custom js for this page -->
     <script src="{{ asset('backend/assets/js/dashboard-dark.js') }}"></script>
+    
     <!-- End custom js for this page -->
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+    <script>
+     @if(Session::has('message'))
+     var type = "{{ Session::get('alert-type','info') }}"
+     switch(type){
+        case 'info':
+        toastr.info(" {{ Session::get('message') }} ");
+        break;
+    
+        case 'success':
+        toastr.success(" {{ Session::get('message') }} ");
+        break;
+    
+        case 'warning':
+        toastr.warning(" {{ Session::get('message') }} ");
+        break;
+    
+        case 'error':
+        toastr.error(" {{ Session::get('message') }} ");
+        break; 
+     }
+     @endif 
+    </script>
 
 </body>
 
